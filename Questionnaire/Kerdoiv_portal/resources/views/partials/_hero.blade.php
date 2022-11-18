ff01c5e569a8f5f24cd1e2b733a518c1a23fc0b4
safran3k <futo.ricsi@gmail.com>
2022-11-18 12:19:00 +0100

    hero section section/search form létrehozása

---
+++ Questionnaire/Kerdoiv_portal/resources/views/partials/_hero.blade.php
@@ -0,0 +1,15 @@
+<!-- Hero -->
+<section class="relative h-72 bg-cyan-600 flex flex-col justify-center align-center text-center space-y-4 mb-4">
+    <div class="absolute top-0 left-0 w-full h-full opacity-10 bg-repeat-0 bg-left"
+        style="background-image: url('images/clipboardleft.png')"></div>
+
+
+    <div class="z-10">
+        <h1 class="text-6xl font-bold uppercase text-white">
+            Kérdőív<span class="text-black">Portál</span>
+        </h1>
+        <p class="text-2xl text-gray-200 font-bold my-4">
+            Kérdőívek kitöltése & létrehozása
+        </p>
+    </div>
+</section>
