5d9fe46683cbea69bfc79ced1a028f2f4650bec4
safran3k <futo.ricsi@gmail.com>
2022-11-21 09:47:47 +0100

    Kerdoiv model módosítása Survey modelre/Survey model megírása

--- Questionnaire/Kerdoiv_portal/app/Models/Survey.php
+++ Questionnaire/Kerdoiv_portal/app/Models/Survey.php
@@ -5,15 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Kerdoiv extends Model
+class Survey extends Model
 {
     use HasFactory;
 
     public function scopeFilter($query, array $filters){
 
-        if($filters["search"] ?? false){
-            $query->where("cim", "like", "%" . request("search") . "%")
-            ;
+        if ($filters['search'] ?? false) {
+            $query->where('cim', 'like', '%' . request('search') . '%') 
+            ->orWhere('rovid_leiras', 'like', '%' . request('search') . '%');
         }
     }
 }
