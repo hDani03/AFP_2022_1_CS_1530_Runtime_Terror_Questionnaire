595f081e9b844849befe223eafdbeb1185a61a9f
safran3k <futo.ricsi@gmail.com>
2022-11-21 09:54:19 +0100

    KerdoivController módosítása SurveyController-re/SurveyController megírása

---
+++ Questionnaire/Kerdoiv_portal/app/Http/Controllers/SurveyController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SurveyController extends Controller
+{
+    // Összes kérdőív mutatása
+    public function index(){
+        // Lehet nem itt kellene
+        // if (Auth::check()) {
+        //     return view('surveys.index', [
+        //         'surveys' => Survey::latest()->filter(request(['search']))->paginate(6) 
+        //         //paginate()->lastItem() regisztrálatlan felhasználóknak
+        //     ]);
+        // }else {
+        //     return view('surveys.index', [
+        //         'surveys' => Survey::latest()->filter(request(['search']))->paginate()->lastItem()
+        //         //paginate()->lastItem() regisztrálatlan felhasználóknak
+        //     ]);
+        // }
+
+        return view('surveys.index', [
+            'surveys' => Survey::latest()->filter(request(['search']))->paginate(6)
+            //paginate()->lastItem() regisztrálatlan felhasználóknak
+        ]);
+        
+    }
+}
