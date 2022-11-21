b07f8e95dcf8db70fc0c83748b1bc4adfb56461b
safran3k <futo.ricsi@gmail.com>
2022-11-21 09:51:49 +0100

    UserController módosítása

--- Questionnaire/Kerdoiv_portal/app/Http/Controllers/UserController.php
+++ Questionnaire/Kerdoiv_portal/app/Http/Controllers/UserController.php
@@ -10,70 +10,64 @@ class UserController extends Controller
 {
     //REGISZTRÁCIÓ
 
-    //Show Register/Create Form
-    public function create()
-    {
+    // Regisztráció/Létrehozás form megjelenítése
+    public function create(){
         return view('users.register');
     }
 
-    public function store(Request $request)
-    {
+    // Új felhasználó létrehozása
+    public function store(Request $request){
+
         $formFields = $request->validate([
             'name' => ['required', 'min:3'],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password' => 'required|confirmed|min:6'
-
         ]);
 
-        //Hash password
+        // Jelszó hash
         $formFields['password'] = bcrypt($formFields['password']);
 
-        //Create User
+        // Felhasználó létrehozása
         $user = User::create($formFields);
 
-        //Login
+        // Bejelentkezés
         auth()->login($user);
-        return redirect('/kerdoivek')->with('message', 'A felhasználó létrehozva, kérjük jelentkezzen be');
+
+        return redirect('/')->with('message', 'Felhasználó létrehozva és bejeletkezve!');
     }
 
     //BEJELENTKEZÉS
 
     // Felhasználó kijelentkeztetése
     public function logout(Request $request){
+
         auth()->logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-    
-        return redirect("/")->with("message", "Sikeres kijelentkezés!");
+
+        return redirect('/')->with('message', 'Kijelentkezett.');
     }
 
-    // Logint form
-    public function login()
-    {
-        return view("users.login");
+    // Bejelentkező form megjelenítése
+    public function login(){
+        return view('users.login');
     }
 
-    // User autentikációja (regisztrált felhasználók)
-    public function authenticate(Request $request)
-    {
-        $formField = $request->validate([
-            "email" => ["required", "email"],
-            "password" => "required"
+    // Felhasználó hitelesítése
+    public function authenticate(Request $request){
+
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
         ]);
 
-        if (auth()->attempt($formField)) {
+        if (auth()->attempt($formFields)) {
             $request->session()->regenerate();
 
-            return redirect("/kerdoivek")->with("message", "Sikeres bejelentkezés!");
+            return redirect('/')->with('message', 'Sikeres bejelentkezés.');
         }
 
-        return back()->withErrors(["email" => "Érvénytelen adatok"])->onlyInput("email");
-    }
-
-    // Belépés vendégként
-    public function loginasguest()
-    {
-        return view("/kerdoivek")->with("message", "Vendégként lépett be.");
+        return back()->withErrors(['email' => 'Helytelen adatok!'])->onlyInput('email');
     }
 }
