5707b072d6647057fc7256ff694f43421e846b58
safran3k <futo.ricsi@gmail.com>
2022-11-22 18:11:21 +0100

    show.blade.php kialakításának módosítása

--- Questionnaire/Kerdoiv_portal/resources/views/surveys/show.blade.php
+++ Questionnaire/Kerdoiv_portal/resources/views/surveys/show.blade.php
@@ -13,23 +13,59 @@
                 <h1 class="text-4xl mb-2 font-bold"> {{ $survey->cim }} </h1>
             </div>
 
-            <div class="my-10" />
 
             <form method="POST" action="" class="w-full max-w-sm">
                 <div class="grid grid-cols-1 divide-y">
 
-                    <h2 class="item-left justify-center font-bold text-2xl"> 1. Kérdés </h2>
+                    <h2 class="justify-center font-bold text-3xl"> 1. Kérdés </h2>
+                    <p class="justify-center font-bold text-2xl pt-5"> Ez egy kérdés? </p>
 
                     <div class="flex flex-col items-left justify-center text-center">
                         <div class="flex item-stretch text-xl">
+                            <legend class="sr-only"> answers </legend>
 
-                            <div class="py-4 mr-6"> Válasz 1 </div>
-                            <div class="py-4 mr-6"> Válasz 2 </div>
-                            <div class="py-4 mr-6"> Válasz 3 </div>
-                            <div class="py-4 mr-6"> Válasz 4 </div>
+                            <div class="py-4 mr-6">
+                                <div class="flex items-center mb-4">
+                                    <input id="answer_option_1" name="answers" type="radio" value=""
+                                        class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" checked="">
+                                    <label for="option_1" class="text-lg font-medium text-gray-900 ml-2 block">
+                                        Igen
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="py-4 mr-6">
+                                <div class="flex items-center mb-4">
+                                    <input id="answer_option_2" name="answers" type="radio" value=""
+                                        class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" checked="">
+                                    <label for="option_2" class="text-lg font-medium text-gray-900 ml-2 block">
+                                        Nem
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="py-4 mr-6">
+                                <div class="flex items-center mb-4">
+                                    <input id="answer_option_3" name="answers" type="radio" value=""
+                                        class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" checked="">
+                                    <label for="option_3" class="text-lg font-medium text-gray-900 ml-2 block">
+                                        Villamos
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="py-4 mr-6">
+                                <div class="flex items-center mb-4">
+                                    <input id="answer_option_4" name="answers" type="radio" value=""
+                                        class="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" checked="">
+                                    <label for="option_4" class="text-lg font-medium text-gray-900 ml-2 block">
+                                        Kecske
+                                    </label>
+                                </div>
+                            </div>
 
                         </div>
-                        <!-- KÉRDÉSEK VÁLASZOK BEJÁRÁSA -->
+                        <!-- KÉRDÉSEK VÁLASZOK KIÍRÁSA -->
                     </div>
                 </div>
                 <button type="submit" class="bg-cyan-600 text-white rounded py-2 px-4 hover:bg-black">
