d7698698983ef89cdf98a5c618fabce30ca24700
turmxhun <davidbiesz13@gmail.com>
2022-11-27 13:54:54 +0100

    Questions és Answers táblafeltöltés átvariálása
    (Működik mind a 2)

--- Questionnaire/Kerdoiv_portal/app/Http/Controllers/SurveyController.php
+++ Questionnaire/Kerdoiv_portal/app/Http/Controllers/SurveyController.php
@@ -29,26 +29,39 @@ class SurveyController extends Controller
     public function store(Request $request)
     {
         $dt = new DateTime(now());
-        $statement  = DB::select("SHOW TABLE STATUS LIKE 'surveys'");
-        $nextSurveyId = $statement[0]->Auto_increment;
+        $nextSurveyIdStatement  = DB::select("SHOW TABLE STATUS LIKE 'surveys'");
+        $nextSurveyId = $nextSurveyIdStatement[0]->Auto_increment;
+
+
+        $questions = $request['question'];
+        $answers = array($request['answer1'], $request['answer2'], $request['answer3'], $request['answer4']);
+
+
         $formFields = $request->validate([
             'cim' => 'required',
             'leiras' => 'required',
         ]);
+
+
+        //Surveys tábla
         DB::table('surveys')->insert(
             array(
                 'survey_id' => $nextSurveyId,
-                'user_id' => auth()->id(), 
-                'cim' => $formFields['cim'], 
+                'user_id' => auth()->id(),
+                'cim' => $formFields['cim'],
                 'rovid_leiras' => $formFields['leiras'],
                 'created_at' => $dt,
                 'updated_at' => $dt
-                )
+            )
         );
-        $questions = $request['question'];
 
-        foreach($questions as $question)
-        {
+
+        //Questions és Answers tábla
+        $i = 0;
+        foreach ($questions as $question) {
+            //Questions tábla
+            $nextQuestionIdStatement = DB::select("SHOW TABLE STATUS LIKE 'questions'");
+            $nextQuestionId = $nextQuestionIdStatement[0]->Auto_increment;
             DB::table('questions')->insert(
                 array(
                     'survey_id' => $nextSurveyId,
@@ -58,11 +71,26 @@ class SurveyController extends Controller
                     'updated_at' => $dt
                 )
             );
+
+            //Answers tábla
+            DB::table('answers')->insert(
+                array(
+                    'survey_id' => $nextSurveyId,
+                    'user_id' => auth()->id(),
+                    'question_id' => $nextQuestionId,
+                    'valasz1' => $answers[0][$i],
+                    'valasz2' => $answers[1][$i],
+                    'valasz3' => $answers[2][$i],
+                    'valasz4' => $answers[3][$i],
+                    'created_at' => $dt,
+                    'updated_at' => $dt
+                )
+            );
+            $i++;
         }
 
+        //Answers tábla
         return redirect('/')->with('message', 'A kérdőív sikeresen létrehozva!');
-
-
     }
 
     // Egy kérdőív mutatása
