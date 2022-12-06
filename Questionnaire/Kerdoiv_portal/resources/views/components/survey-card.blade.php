cb2eeabf8391a578f9f64b40587cb86e44840de1
turmxhun <davidbiesz13@gmail.com>
2022-12-06 19:47:57 +0100

    Felhasználónak új tulajdonság -> admin
    Csak admin felhasználóknak jelenik meg a statisztika gomb

--- Questionnaire/Kerdoiv_portal/resources/views/components/survey-card.blade.php
+++ Questionnaire/Kerdoiv_portal/resources/views/components/survey-card.blade.php
@@ -10,7 +10,7 @@
             </h2>
             <div class="text-xl mb-4"> {{ $survey->rovid_leiras }} </div>
 
-            {{-- Ezt a gombot admin jogkörhöz kötni, ha ő jelentkezett be, neki látszódjon, most csak azt nézem hogy ha valaki bejelentkezik --}}
+            {{-- Ezt a gombot admin jogkörhöz kötni, ha ő jelentkezett be, neki látszódjon, most csak azt nézem hogy ha valaki bejelentkezik 
             @auth
                 <div class="absolute h-32 w-32">
                     <a href="/surveys/{{ $survey->survey_id }}/statistic">
@@ -21,6 +21,20 @@
                     </a>
                 </div>
             @endauth
+            --}}
+            @auth
+                @if (Auth::user()->isAdmin)
+                    <div class="absolute h-32 w-32">
+                        <a href="/surveys/{{ $survey->survey_id }}/statistic">
+                            <button
+                                class="fa fa-pie-chart bg-cyan-600 text-white rounded py-2 px-4 hover:bg-black relative bottom-0 left-0 h-12 w-12"
+                                aria-hidden="true">
+                            </button>
+                        </a>
+                    </div>
+                @endif
+            @endauth
+
 
         </div>
 
