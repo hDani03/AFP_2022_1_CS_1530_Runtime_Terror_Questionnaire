5b0fdd10ddbad1112acd3f68b54eece9e0f87af5
hDani03 <danielhuszka1@gmail.com>
2022-12-08 15:54:53 +0100

    Statisztika Stilus javitas

--- Questionnaire/Kerdoiv_portal/resources/views/components/survey-card.blade.php
+++ Questionnaire/Kerdoiv_portal/resources/views/components/survey-card.blade.php
@@ -1,20 +1,20 @@
 @props(['survey'])
 
 <x-card>
-    <div class="flex">
+    <div class="flex h-36">
         <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/clipboard.png') }}" alt="" />
         <div>
             <h2 class="text-2xl font-bold">
                 <a href="/surveys/{{ $survey->survey_id }}"> {{ $survey->cim }} </a>
             </h2>
-            <div class="text-xl mb-4"> {{ $survey->rovid_leiras }} </div>
+            <div class="xxs: text-xl mb-3 md:text-xl mb-4"> {{ $survey->rovid_leiras }} </div>
 
             @auth
                 @if (Auth::user()->isAdmin)
-                    <div class="absolute h-32 w-32">
+                    <div class="xxs: h-24 w-24 md:absolute h-32 w-32">
                         <a href="/surveys/{{ $survey->survey_id }}/statistic">
                             <button
-                                class="fa fa-pie-chart bg-cyan-600 text-white rounded py-2 px-4 hover:bg-black relative bottom-0 left-0 h-12 w-12"
+                                class="fa fa-pie-chart bg-cyan-600 text-white rounded py-2 px-4 hover:bg-black relative bottom-0 left-0 h-12 w-12 mt-2"
                                 aria-hidden="true">
                             </button>
                         </a>
